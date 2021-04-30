b60b3f69f15d497eddd9c00af338ecef5a6e7eaa
Pooja <pooja.potadar@gmail.com>
2021-04-29 19:50:14 -0500

    Basic wireframes

--- site_ems/forms.py
+++ site_ems/forms.py
@@ -5,3 +5,7 @@ class CompanydetailForm(forms.ModelForm):
     class Meta:
         model = company_details
         fields = "__all__"
+
+        widget = {
+            'country': forms.TextInput(attrs={'class': 'form-control'})
+        }
